13d1cb59a04b63acaaafee23b1e69733c2ff1fe3
knowsarzehmeh <osarzeh@gmail.com>
2018-09-15 18:18:24 +0100

    removed dummy expense data

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import "react-dates/lib/css/_datepicker.css";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { addExpense } from "./redux/actions/expenses";
+// import { addExpense } from "./redux/actions/expenses";
 const store = configureStore(); // opened the redux store
 
-store.dispatch(addExpense({ description: "Water Bill" }));
-store.dispatch(addExpense({ description: "Gas Bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "Rent", amount: 100000 }));
+// store.dispatch(addExpense({ description: "Water Bill" }));
+// store.dispatch(addExpense({ description: "Gas Bill", createdAt: 1000 }));
+// store.dispatch(addExpense({ description: "Rent", amount: 100000 }));
 
 console.log(store.getState());
 
